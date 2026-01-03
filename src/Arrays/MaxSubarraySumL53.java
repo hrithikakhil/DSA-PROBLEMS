f21b19deda4450d4f9a5bcc60e9daddd996e628f
Hrithik Manjunath <hrithikakhil18@gmail.com>
2026-01-04 02:18:27 +0530

    Maximum Subarray Sum - 53

--- src/Arrays/MaxSubarraySumL53.java
+++ src/Arrays/MaxSubarraySumL53.java
@@ -2,18 +2,26 @@ package Arrays;
 
 public class MaxSubarraySumL53 {
 
-    public int maxSubArray(int[] nums) {
-        // Kadanes Algo
-        int n = nums.length;
-        int sum = 0;
-        int maxSum = Integer.MIN_VALUE;;
-        for(int i=0; i<n; i++){
-            sum+=nums[i];
-            maxSum = Math.max(sum, maxSum);
-            if(sum<=0){
-                sum = 0;
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int n = nums.length;
+            int maxSum = Integer.MIN_VALUE;
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                sum+=nums[i];
+                maxSum = Math.max(maxSum, sum);
+                if(sum<=0){
+                    sum=0;
+                }
             }
+            return maxSum;
         }
-        return maxSum;
     }
 }
+
+/* kadanes Algo
+At every index:
+-->Either extend the previous subarray
+-->Or start a new subarray from this element
+-->If the previous sum becomes negative, drop it — it only hurts future sums.
+*/
